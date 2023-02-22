085f75c57781ea0706179f706a3ed8d7e498446f
Danila Kuzin <danila_kuzin_2001@mail.ru>
2023-02-22 18:38:01 +0400

    Создание проекта fastApi для реализации интернет магазина

---
+++ db.py
@@ -0,0 +1,7 @@
+import sqlalchemy
+from decouple import config
+import databases
+
+DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:5432/complaints"
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
