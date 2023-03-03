da468a80441ccb9a6efa21d943e051e0afacda1e
Danila Kuzin <danila_kuzin_2001@mail.ru>
2023-03-04 02:49:45 +0400

    Настройка CORS.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from db import database
 from resources.routes import api_router
 
+
+origin = [
+    "http://localhost:8000",
+    "http://127.0.0.1",
+]
+
+
 app = FastAPI()
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=True,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")  # Подключение к базе данных
